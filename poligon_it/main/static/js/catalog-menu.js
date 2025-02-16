299feb390347921be45c78fad1c0e4fcd6675431
Vladimir Telniy <fdavisarz10@gmail.com>
2025-02-16 23:29:14 +0300

    add fixed categories in navbar and add categories carousel and images on main paige

--- poligon_it/main/static/js/catalog-menu.js
+++ poligon_it/main/static/js/catalog-menu.js
@@ -1,17 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
     const megaMenuToggle = document.getElementById("megaMenuToggle");
     const megaMenuContent = document.querySelector(".megamenu-content");
+
+    // Начальные стили для анимации
+    megaMenuContent.style.opacity = "0";
+    megaMenuContent.style.transform = "translateY(-10px)";
     megaMenuContent.style.display = "none";
+    megaMenuContent.style.transition = "opacity 0.3s ease, transform 0.3s ease";
+
     megaMenuToggle.addEventListener("mouseenter", function () {
         megaMenuContent.style.display = "flex";
+        setTimeout(() => {
+            megaMenuContent.style.opacity = "1";
+            megaMenuContent.style.transform = "translateY(0)";
+        }, 10);
     });
 
     megaMenuContent.addEventListener("mouseleave", function () {
-        megaMenuContent.style.display = "none";
+        megaMenuContent.style.opacity = "0";
+        megaMenuContent.style.transform = "translateY(-10px)";
+        setTimeout(() => {
+            megaMenuContent.style.display = "none";
+        }, 300);
     });
+
     document.addEventListener("click", function (event) {
         if (!megaMenuToggle.contains(event.target) && !megaMenuContent.contains(event.target)) {
-            megaMenuContent.style.display = "none";
+            megaMenuContent.style.opacity = "0";
+            megaMenuContent.style.transform = "translateY(-10px)";
+            setTimeout(() => {
+                megaMenuContent.style.display = "none";
+            }, 300);
         }
     });
 });
---
+++ poligon_it/main/static/js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.getElementById("carousel");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    
+    let scrollAmount = 0;
+    const scrollStep = 200;
+    
+    nextBtn.addEventListener("click", function () {
+        carousel.scrollBy({ left: scrollStep, behavior: "smooth" });
+    });
+
+    prevBtn.addEventListener("click", function () {
+        carousel.scrollBy({ left: -scrollStep, behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('slider load')
+    const slider = document.querySelector(".slider");
+    const slides = document.querySelectorAll(".slide");
+    let index = 0;
+
+    function nextSlide() {
+        index = (index + 1) % slides.length;
+        slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(nextSlide, 5000);
+});
